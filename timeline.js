18fa5be7a70c9e54f4f373bcc6511e6440c5c515
oskip123 <56744714+oskip123@users.noreply.github.com>
2022-08-05 18:54:00 -0400

    yes

--- timeline.js
+++ timeline.js
@@ -5,29 +5,33 @@ tested with jQuery v1.6+
 Copyright 2011, CSSLab.cl
 Free under the MIT license.
 https://www.opensource.org/licenses/mit-license.php
-
-source: http://www.csslab.cl/2011/08/18/jquery-timelinr/
 ---------------------------------- */
+
+$(function(){
+	$().timelinr({
+	arrowKeys: 'true'
+	})
+});
 
 jQuery.fn.timelinr = function(options){
 	settings = jQuery.extend({
-		orientation: 				'horizontal',		// value: horizontal | vertical, default to horizontal
-		containerDiv: 				'#timeline',		// value: any HTML tag or #id, default to #timeline
-		datesDiv: 					'#dates',			// value: any HTML tag or #id, default to #dates
-		datesSelectedClass: 		'selected',			// value: any class, default to selected
-		datesSpeed: 				'normal',			// value: integer between 100 and 1000 (recommended) or 'slow', 'normal' or 'fast'; default to normal
-		yearsDiv: 					'#years',			// value: any HTML tag or #id, default to #years
-		yearsSelectedClass: 		'selected',			// value: any class, default to selected
-		yearsSpeed: 				'fast',				// value: integer between 100 and 1000 (recommended) or 'slow', 'normal' or 'fast'; default to fast
-		yearsTransparency: 		0.2,				// value: integer between 0 and 1 (recommended), default to 0.2
-		yearsTransparencySpeed: 	500,				// value: integer between 100 and 1000 (recommended), default to 500 (normal)
-		prevButton: 				'#prev',			// value: any HTML tag or #id, default to #prev
-		nextButton: 				'#next',			// value: any HTML tag or #id, default to #next
-		arrowKeys: 					'false',			// value: true | false, default to false
-		startAt: 					1,					// value: integer, default to 1 (first)
-		autoPlay: 					'false',			// value: true | false, default to false
-		autoPlayDirection: 			'forward',			// value: forward | backward, default to forward
-		autoPlayPause: 				2000				// value: integer (1000 = 1 seg), default to 2000 (2segs)
+		orientation: 				'horizontal',	
+		containerDiv: 				'#timeline',		
+		datesDiv: 					'#dates',		
+		datesSelectedClass: 		'selected',			
+		datesSpeed: 				'normal',			
+		yearsDiv: 					'#years',		
+		yearsSelectedClass: 		'selected',			
+		yearsSpeed: 				'fast',				
+		yearsTransparency: 		0.2,				
+		yearsTransparencySpeed: 	500,				
+		prevButton: 				'#prev',			
+		nextButton: 				'#next',			
+		arrowKeys: 					'false',	
+		startAt: 					1,					
+		autoPlay: 					'false',		
+		autoPlayDirection: 			'forward',			
+		autoPlayPause: 				2000			
 	}, options);
 
 	$(function(){
