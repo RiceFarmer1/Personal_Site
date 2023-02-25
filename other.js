455c3e6a1386d80664960247756d1f931a7b7865
ricefarmer1 <oskioscar135@gmail.com>
2023-02-25 11:18:38 -0500

    yes

--- other.js
+++ other.js
@@ -1,9 +1,9 @@
-function w3_open() {
+function open_menu() {
     document.getElementById("mySidebar").style.display = "block";
     document.getElementById("myOverlay").style.display = "block";
 }
 
-function w3_close() {
+function close_menu() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
 }
