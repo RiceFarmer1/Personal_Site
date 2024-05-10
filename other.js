98a2afd4977c774be7c35815710df1f971abe2d7
oskip0906 <oscar.pang050906@gmail.com>
2024-05-10 03:06:00 -0400

    major changes

--- other.js
+++ other.js
@@ -1,14 +1,20 @@
-function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
-    var captionText = document.getElementById("caption");
-    captionText.innerHTML = element.alt;
-}
+function toggleButton() {
+  var sidebar = document.getElementById('sidebar');
+  var content = document.getElementById('content');
+  if (sidebar.classList.contains('sidebar-closed')) {
+      sidebar.classList.remove('sidebar-closed');
+      content.classList.remove('content-shift');
+  } else {
+      sidebar.classList.add('sidebar-closed');
+      content.classList.add('content-shift');
+  }
+};
 
 function playAudio() {
   var audio = document.getElementById("audio");
   audio.play();
 }
+
 function pauseAudio() {
   var audio = document.getElementById("audio");
   audio.pause();
